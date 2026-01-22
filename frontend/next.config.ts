02c737090aee8da25dc93300afef4507aaf2891c
Dmytro <guladmitro261@gmail.com>
2026-01-22 10:23:17 +0100

    commit final

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,14 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig = {
   async rewrites() {
     return [
-      // 1. СЕСІЇ (Не чіпаємо)
       {
         source: '/api/session/:path*',
         destination: 'http://session_service:8000/api/session/:path*',
       },
+      {
+        source: '/api/text/verify',
+        destination: 'http://text_service:8000/api/text/verify',
+      },
 
-      // 2. AI ЗАПИТИ (Додаємо ПЕРЕД текстом, щоб вони мали пріоритет)
-      // Ми направляємо їх на ai_service:8000, як каже твій Docker
       {
         source: '/api/text/text_correction',
         destination: 'http://ai_service:8000/api/v1/text/text_correction',
@@ -20,7 +21,6 @@ const nextConfig = {
         destination: 'http://ai_service:8000/api/v1/text/text_summarization',
       },
 
-      // 3. ТЕКСТ АПІ (Твоя робоча логіка)
       {
         source: '/api/text',
         destination: 'http://text_service:8000/api/text/', 
@@ -33,7 +33,6 @@ const nextConfig = {
         source: '/api/text/:path*',
         destination: 'http://text_service:8000/api/v1/text/:path*', 
       },
-      // Це правило /api/ai/ на всякий випадок, якщо будеш стукати прямо в AI
       {
         source: '/api/ai/:path*',
         destination: 'http://ai_service:8000/api/v1/ai/:path*',
